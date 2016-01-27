cbf8dd38be86c819e3c810ccfb7d259f9ec58857
Ke Liu <Ke Liu>
2016-01-27 23:55:12 +0800

    Initial commit

---
+++ app/src/main/java/com/itheima/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.itheima.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("花姑娘.....");
+
+        System.out.println("我是一个兵....");
+        System.out.println("我是一个兵....");
+        System.out.println("我是一个兵....");
+        System.out.println("我是一个兵....");
+
+
+        System.out.println("爱上来得及==========");
+        System.out.println("爱上来得及==========");
+        System.out.println("爱上来得及==========");
+        System.out.println("爱上来得及==========");
+        System.out.println("爱上来得及==========");
+        System.out.println("爱上来得及==========");
+    }
+}
